d92e79997f2d7fc8c0a7f540165f080c33dc6e2f
Sandor Boros <sandor@Sandors-MacBook-Pro.local>
2015-05-06 14:44:53 +0200

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Sandor Boros on 06/05/15.
+//  Copyright (c) 2015 Sandor Boros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Sandor Boros on 06/05/15.
+//  Copyright (c) 2015 Sandor Boros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
